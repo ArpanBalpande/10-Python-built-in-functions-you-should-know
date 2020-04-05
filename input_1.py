da1a2fb486c794e0bd172b977d1f7c740cb162f3
Arpan Balpande <arpan.balpande22@gmail.com>
2020-04-05 19:29:34 +0530

    added input function

---
+++ input_1.py
@@ -0,0 +1,6 @@
+# ask the user to input a name
+name = input('Enter your name: ')
+
+# use the name in the following greeting
+print('Hello {}'.format(name))
+# Hello Amanda
---
+++ input_2.py
@@ -0,0 +1,11 @@
+# ask the user to input a number
+number_1 = float(input('Enter a number: '))
+# Enter a number: 2.5
+
+# ask the user to input another number
+number_2 = float(input('Enter another number: '))
+# Enter another number: 3.4
+
+# print the sum of both numbers
+print('The sum is: {}'.format(number_1 + number_2))
+# The sum is: 5.9
